c5eb3dd8680fed87da66ac65ac92947aa15131cd
Hugo <u201515078@gmail.com>
2018-03-21 13:47:01 -0500

    add class Operaciones/Aritmetica

---
+++ Semana 1/Operaciones/Aritmetica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    public class Aritmetica
+    {
+        public Int32 sumar(Int32 A, Int32 B) => A + B;
+        public Int32 restar(Int32 A, Int32 B) => A - B;
+        public Int32 multiplicar(Int32 A, Int32 B) => A * B;
+        public Int32 dividir(Int32 A, Int32 B) => A / B;
+    }
+}
